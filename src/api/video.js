46534ea5c143e1838bcd4b662ad09fbd2be90d24
Bibek Sah <bibekkumarsah.456@gmail.com>
2025-06-18 15:31:38 +0545

    vite.config adding for scalable import and export component

--- src/api/video.js
+++ src/api/video.js
@@ -1,3 +1,11 @@
- export const getAllVideos = () => {
-  
+import { axiosInstance } from "@/config/axiosInstance"
+
+ export const getAllVideos =async () => {
+  try {
+    const { data } = await axiosInstance.get("/videos")
+    console.log(data)
+    return data
+  } catch (error) {
+    console.log("Something went wrong !",error)
+  }
 }
